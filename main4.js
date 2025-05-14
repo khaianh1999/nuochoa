6ad0a0227b4914b29fe59c3a7797128f2f704e00
khaianh <khaianh@192.168.1.8.non-exists.ptr.local>
2025-05-14 22:33:10 +0700

    main

---
+++ main4.js
@@ -0,0 +1,24 @@
+(function () { var run = function () { 
+    if (typeof window.LadiPageScript == "undefined" || typeof window.ladi == "undefined" || window.ladi == undefined)
+         { setTimeout(run, 100); return; } 
+    window.LadiPageApp = window.LadiPageApp || new window.LadiPageAppV2(); 
+    window.LadiPageScript.runtime.ladipage_id = '64733af8ba114c001215fc31';
+     window.LadiPageScript.runtime.publish_platform = 'LADIPAGEDNS';
+      window.LadiPageScript.runtime.version = '1725620447318'; 
+      window.LadiPageScript.runtime.cdn_url = 'https://w.ladicdn.com/v4/source/';
+       window.LadiPageScript.runtime.DOMAIN_SET_COOKIE = ["tuybutky.com"];
+        window.LadiPageScript.runtime.DOMAIN_FREE = ["preview.ldpdemo.com", "ldp.page"]; 
+        window.LadiPageScript.runtime.bodyFontSize = 12; 
+        window.LadiPageScript.runtime.store_id = "5e51ff6161a254079fc6d2ed"; 
+        window.LadiPageScript.runtime.time_zone = 7; 
+        window.LadiPageScript.runtime.currency = "VND";
+         window.LadiPageScript.runtime.convert_replace_str = true;
+          window.LadiPageScript.runtime.desktop_width = 1200;
+           window.LadiPageScript.runtime.mobile_width = 420;
+            window.LadiPageScript.runtime.tracking_global_delay = 0;
+             window.LadiPageScript.runtime.formdata = true;
+              window.LadiPageScript.runtime.tracking_button_click = true; 
+              window.LadiPageScript.runtime.publish_time = 1725699490132; 
+              window.LadiPageScript.runtime.lang = "vi";
+               window.LadiPageScript.run(true);
+                window.LadiPageScript.runEventScroll(); }; run(); })();
